ca17ab6c8a54f0dd1977f86e54c7e4b371e2ac3c
Sangwan Jeon <commits@swjeon.kr>
2023-06-08 11:11:34 +0000

    fix: return url

--- src/app/(admin)/admin/(full)/posts/[id]/Editor.tsx
+++ src/app/(admin)/admin/(full)/posts/[id]/Editor.tsx
@@ -32,7 +32,7 @@ export default function Editor({ post }: Props) {
   return (
     <>
       <div className="w-full flex p-3 border-b border-gray-100">
-        <a href="/admin/drafts" className="flex items-center">
+        <a href="/admin/posts" className="flex items-center">
           <IconChevronLeft className="w-5 h-5 mr-2" />
           <p>Return to post list</p>
         </a>
